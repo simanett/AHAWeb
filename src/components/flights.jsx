cc057029dfd3f0572cd73d306f6cf17a3b05d010
Bernot <helga.bernot@nokia.com>
2016-10-22 23:02:42 +0200

    Promise

--- src/components/flights.jsx
+++ src/components/flights.jsx
@@ -9,61 +9,69 @@ import { Table } from 'react-bootstrap';
 var flights = require("../api/flightsData");
 
 function getFlightsFromServlet() {
-    $.ajax({
-        cache: false,
-        dataType: "json",
-        error: function (xhr, status, err) {
-            console.log("ajax error");
-        }.bind(this),
-        success: function (data) {
-            console.log("ajax success");
-            console.log(data);
-            return data;
-        }.bind(this),
-        url: "https://www.foaas.com/gfy/Anett/Helga",
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            cache: false,
+            dataType: "json",
+            error: function (xhr, status, err) {
+                console.log("ajax error");
+                reject(err);
+            }.bind(this),
+            success: function (data) {
+                console.log("ajax success");
+                console.log(data);
+                resolve(data);
+            }.bind(this),
+            url: "https://www.foaas.com/gfy/Anett/Helga",
+        });
     });
 }
 
 export default class Flights extends React.Component {
+
     render() {
-        // var jsonData = getFlightsFromServlet();
-        var jsonData = {message: "Golf foxtrot yankee, Anett.", subtitle: "- Helga"};
-        return (
-            <div id="flights">
-                <h2>Flights</h2>
-                <div key={jsonData.message}>{ jsonData.message }</div>
-                <Table bordered hover responsive striped>
-                    <thead>
-                        <tr>
-                            <th>Id</th>
-                            <th>Flight number</th>
-                            <th>Departure</th>
-                            <th>Flight duration</th>
-                            <th>Airplane</th>
-                            <th>From</th>
-                            <th>To</th>
-                            <th>Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { flights.getFlights().map((flight, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td>{ flight.id }</td>
-                                    <td>{ flight.flightNumber }</td>
-                                    <td>{ flight.departure }</td>
-                                    <td>{ flight.flightDuration }</td>
-                                    <td>{ flight.airplane.model }</td>
-                                    <td>{ flight.airportFrom.city }</td>
-                                    <td>{ flight.airportTo.city }</td>
-                                    <td>{ flight.basicPrice }</td>
-                                </tr>
-                            )
-                        }) }
-                    </tbody>
-                </Table>
-            </div>
-        );
+        getFlightsFromServlet().then((result) => {
+            console.log("after promise");
+            return (
+                <div id="flights">
+                    <h2>Flights</h2>
+                    <div key={jsonData.message}>{ jsonData.message }</div>
+                    <Table bordered hover responsive striped>
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Flight number</th>
+                                <th>Departure</th>
+                                <th>Flight duration</th>
+                                <th>Airplane</th>
+                                <th>From</th>
+                                <th>To</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { flights.getFlights().map((flight, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{ flight.id }</td>
+                                        <td>{ flight.flightNumber }</td>
+                                        <td>{ flight.departure }</td>
+                                        <td>{ flight.flightDuration }</td>
+                                        <td>{ flight.airplane.model }</td>
+                                        <td>{ flight.airportFrom.city }</td>
+                                        <td>{ flight.airportTo.city }</td>
+                                        <td>{ flight.basicPrice }</td>
+                                    </tr>
+                                )
+                            }) }
+                        </tbody>
+                    </Table>
+                </div>
+            );
+
+        });
+        var jsonData = { message: "Golf foxtrot yankee, Anett.", subtitle: "- Helga" };
+
     }
 }
 
