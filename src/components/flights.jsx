22a70542967599e3abb5f66ccc5d8531f8c7e73b
Bernot <helga.bernot@nokia.com>
2016-10-22 16:56:31 +0200

    display json from ajax GET

--- src/components/flights.jsx
+++ src/components/flights.jsx
@@ -1,18 +1,37 @@
 import React from 'react';
+import * as $ from 'jquery';
 /*
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 */
 import { Table } from 'react-bootstrap';
 
-// var flights = require("../api/flights");
 var flights = require("../api/flightsData");
+
+function getFlightsFromServlet() {
+    $.ajax({
+        cache: false,
+        dataType: "json",
+        error: function (xhr, status, err) {
+            console.log("ajax error");
+        }.bind(this),
+        success: function (data) {
+            console.log("ajax success");
+            console.log(data);
+            return data;
+        }.bind(this),
+        url: "https://www.foaas.com/gfy/Anett/Helga",
+    });
+}
 
 export default class Flights extends React.Component {
     render() {
+        // var jsonData = getFlightsFromServlet();
+        var jsonData = {message: "Golf foxtrot yankee, Anett.", subtitle: "- Helga"};
         return (
             <div id="flights">
-            <h2>Flights</h2>
+                <h2>Flights</h2>
+                <div key={jsonData.message}>{ jsonData.message }</div>
                 <Table bordered hover responsive striped>
                     <thead>
                         <tr>
@@ -29,16 +48,16 @@ export default class Flights extends React.Component {
                     <tbody>
                         { flights.getFlights().map((flight, index) => {
                             return (
-                            <tr key={index}>
-                                <td>{ flight.id }</td>
-                                <td>{ flight.flightNumber }</td>
-                                <td>{ flight.departure }</td>
-                                <td>{ flight.flightDuration }</td>
-                                <td>{ flight.airplane.model }</td>
-                                <td>{ flight.airportFrom.city }</td>
-                                <td>{ flight.airportTo.city }</td>
-                                <td>{ flight.basicPrice }</td>
-                            </tr>
+                                <tr key={index}>
+                                    <td>{ flight.id }</td>
+                                    <td>{ flight.flightNumber }</td>
+                                    <td>{ flight.departure }</td>
+                                    <td>{ flight.flightDuration }</td>
+                                    <td>{ flight.airplane.model }</td>
+                                    <td>{ flight.airportFrom.city }</td>
+                                    <td>{ flight.airportTo.city }</td>
+                                    <td>{ flight.basicPrice }</td>
+                                </tr>
                             )
                         }) }
                     </tbody>
