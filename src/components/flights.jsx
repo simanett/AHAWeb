c7d6a5a264c67c7368cce8ed3afb62bffb164b4f
Bernot <helga.bernot@nokia.com>
2016-10-23 11:37:38 +0200

    Update displayed data from Promise

--- src/components/flights.jsx
+++ src/components/flights.jsx
@@ -29,49 +29,103 @@ function getFlightsFromServlet() {
 
 export default class Flights extends React.Component {
 
-    render() {
-        getFlightsFromServlet().then((result) => {
-            console.log("after promise");
-            return (
-                <div id="flights">
-                    <h2>Flights</h2>
-                    <div key={jsonData.message}>{ jsonData.message }</div>
-                    <Table bordered hover responsive striped>
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Flight number</th>
-                                <th>Departure</th>
-                                <th>Flight duration</th>
-                                <th>Airplane</th>
-                                <th>From</th>
-                                <th>To</th>
-                                <th>Price</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            { flights.getFlights().map((flight, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{ flight.id }</td>
-                                        <td>{ flight.flightNumber }</td>
-                                        <td>{ flight.departure }</td>
-                                        <td>{ flight.flightDuration }</td>
-                                        <td>{ flight.airplane.model }</td>
-                                        <td>{ flight.airportFrom.city }</td>
-                                        <td>{ flight.airportTo.city }</td>
-                                        <td>{ flight.basicPrice }</td>
-                                    </tr>
-                                )
-                            }) }
-                        </tbody>
-                    </Table>
-                </div>
-            );
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "initial msg",
+            flights: [{
+                "flightNumber": "INIFLIGHT",
+                "departure": "júl. 22, 2016",
+                "flightDuration": 180,
+                "airplane": {
+                    "maxDistance": 10000,
+                    "model": "Airbus A320",
+                    "id": 2
+                },
+                "airportTo": {
+                    "code": "BUD",
+                    "city": "Budapest"
+                },
+                "airportFrom": {
+                    "code": "NAN",
+                    "city": "Nantes"
+                },
+                "id": 2,
+                "basicPrice": 23000
+            },],
+        }
+    }
 
+    componentWillMount() {
+        getFlightsFromServlet().then((result) => {
+            this.setState({
+                message: result.message,
+                flights: [{
+                "flightNumber": "AHA1111",
+                "departure": "júl. 20, 2016",
+                "flightDuration": 30,
+                "airplane": {
+                    "maxDistance": 10000,
+                    "model": "Airbus A320",
+                    "id": 2
+                },
+                "airportTo": {
+                    "code": "BUD",
+                    "city": "Budapest"
+                },
+                "airportFrom": {
+                    "code": "NAN",
+                    "city": "Nantes"
+                },
+                "id": 41,
+                "basicPrice": 45000
+            }],
+            });
         });
+    }
+
+    render() {
+
         var jsonData = { message: "Golf foxtrot yankee, Anett.", subtitle: "- Helga" };
 
+        return (
+            <div id="flights">
+                <h2>Flights</h2>
+                <div key={jsonData.message}>{jsonData.message}</div>
+                <h3>{this.state.message}</h3>
+                <h3>{this.state.flights[0].flightNumber}</h3>
+                <Table bordered hover responsive striped>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Flight number</th>
+                            <th>Departure</th>
+                            <th>Flight duration</th>
+                            <th>Airplane</th>
+                            <th>From</th>
+                            <th>To</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {flights.getFlights().map((flight, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{flight.id}</td>
+                                    <td>{flight.flightNumber}</td>
+                                    <td>{flight.departure}</td>
+                                    <td>{flight.flightDuration}</td>
+                                    <td>{flight.airplane.model}</td>
+                                    <td>{flight.airportFrom.city}</td>
+                                    <td>{flight.airportTo.city}</td>
+                                    <td>{flight.basicPrice}</td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </Table>
+            </div>
+        );
     }
 }
 
