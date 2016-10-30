b94080afb44c85a278ef9642aa8a009ebb0cd4b9
HB <hb@aha.git>
2016-10-30 11:42:35 +0100

    Refactor

--- src/components/flights.jsx
+++ src/components/flights.jsx
@@ -52,7 +52,7 @@ class Flights extends React.Component {
                         <tbody>
                             {this.props.flights.map((flight, index) => {
                                 return (
-                                    <tr key={index} onClick={this.chooseFlight.bind(this)}>
+                                    <tr key={index} onClick={this.chooseFlight.bind(this) }>
                                         <td>{flight.id}</td>
                                         <td>{flight.flightNumber}</td>
                                         <td>{flight.departure}</td>
@@ -71,23 +71,20 @@ class Flights extends React.Component {
         );
     }
 
-    chooseFlight(event){
-        let value = event.target.parentElement.firstChild.textContent;
+    chooseFlight(event) {
         let parent = event.target.parentElement;
-        let chosenFlight = {};
-        chosenFlight.id = parent.cells.item(0).textContent;
-        chosenFlight.flightNumber = parent.cells.item(1).textContent;
-        chosenFlight.departure = parent.cells.item(2).textContent;
-        chosenFlight.flightDuration = parent.cells.item(3).textContent;
-        chosenFlight.airplane = parent.cells.item(4).textContent;
-        chosenFlight.airportFrom = parent.cells.item(5).textContent;
-        chosenFlight.airportTo = parent.cells.item(6).textContent;
-        chosenFlight.basicPrice = parent.cells.item(7).textContent;
-        
-        if(parent){
-            alert("flight chosen" + chosenFlight.id);
+        let chosenFlight = {
+            id: parent.cells.item(0).textContent,
+            flightNumber: parent.cells.item(1).textContent,
+            departure: parent.cells.item(2).textContent,
+            flightDuration: parent.cells.item(3).textContent,
+            airplane: parent.cells.item(4).textContent,
+            airportFrom: parent.cells.item(5).textContent,
+            airportTo: parent.cells.item(6).textContent,
+            basicPrice: parent.cells.item(7).textContent,
+        }
+        if (parent) {
             store.dispatch(Actions.chooseFlight(chosenFlight));
-            // alert("flight chosen" + parent.cells.length);
         }
     }
 }
